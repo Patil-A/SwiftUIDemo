83aa27096ecfa757a59178e911bd2ee6cdc471e9
Ajay Patil <ajay.patil@iaai.com>
2022-03-04 01:32:55 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Ajay Patil on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
